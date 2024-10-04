400d919343a9035f7fe7ca60a5ec9f00cfcf7aee
Sd Inf JORDAN <eb0118706472@eceme.eb.mil.br>
2024-10-04 11:10:57 -0300

    fdhfgd

---
+++ 5_op_e_expressoes/30_exercicio_20/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+$a = 10;
+$b = 20;
+
+echo $a >= $b ? "A é maior ou igual B <br>" : "A NÃO é maior ou igual B <br>";
+
+if($a >= $b){
+    echo "A é maior ou igual B <br>";
+}
+else echo "A NÃO é maior ou igual B <br>";
+?>
