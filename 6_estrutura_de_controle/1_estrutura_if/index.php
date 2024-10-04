864a7d674c51e1401205b99ae69f842b34600a87
Sd Inf JORDAN <eb0118706472@eceme.eb.mil.br>
2024-10-04 11:53:25 -0300

    querimbora

---
+++ 6_estrutura_de_controle/1_estrutura_if/index.php
@@ -0,0 +1,34 @@
+<?php 
+
+// Condicao verdadeira
+if (5 > 2){
+
+    echo "Deu certo! Entrou no if 1 <br>";
+
+}
+
+// Condicao falsa
+if (2 >= 5){
+
+    echo "Não vai entrar no if, porque deu false! <br>";
+
+}
+
+// Utilizar op. logico
+if (10 === 10 && 9 > 3){
+
+    echo "Deu certo! Entrou no if 2 <br>";
+
+}
+
+// variaveis
+$a = 10;
+$b = 5;
+
+$c = "Deu certo entrou no if 3";
+if ($a >= $b){
+
+    echo $c;
+
+}
+?>
---
+++ 6_estrutura_de_controle/2_exercicio_21/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+$a = 5;
+$b = 2;
+$c = "Matheus";
+$d = "Pedro";
+$e = 12;
+$f = 11;
+
+echo $a > $b ? "5 é maior que 2 <br>" : "2 é maior que 5 <br>";
+
+if($c != $d){
+    echo "Matheus é diferente de Pedro <br>";
+}
+else echo "Não são diferentes <br>";
+
+if($e <= $f){
+    echo "12 é menor que 11 <br>";
+}
+else echo "12 é maior que 11 <br>";
+
+
+?>
