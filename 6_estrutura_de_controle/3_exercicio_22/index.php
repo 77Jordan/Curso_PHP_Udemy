09210382623eaedf317f048eac44280aa0404a2e
Jordan Galdino de Araujo <112136516+77Jordan@users.noreply.github.com>
2024-10-07 10:52:18 -0300

    ain

---
+++ 6_estrutura_de_controle/3_exercicio_22/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+$kid = 7;
+$teen = 18;
+$old = 65;
+
+if($kid < 18){
+    echo "Criança é menor de idade <br>";
+}
+
+if($teen >= 18){
+    echo "jovem é maior de idade <br>";
+}
+
+if($old >= 18){
+    echo "Idoso é maior de idade <br>";
+}
+?>
