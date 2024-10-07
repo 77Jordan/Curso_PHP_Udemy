a8222de158a93370745c8fdeabbee416ebc1d681
Jordan Galdino de Araujo <112136516+77Jordan@users.noreply.github.com>
2024-10-07 11:43:59 -0300

    esqueci foi mal

---
+++ 6_estrutura_de_controle/5_exercicio_23/index.php
@@ -0,0 +1,34 @@
+<?php 
+
+$kid = 7;
+$teen = 18;
+$old = 65;
+
+if($kid < 18){
+
+    echo "Criança é menor de idade <br>";
+
+}else{
+
+    echo "Criança é maior de idade";
+}
+
+if($teen >= 18){
+
+    echo "jovem é maior de idade <br>";
+
+}else{
+
+    echo "Jovem é menor de idade";
+}
+
+if($old >= 18){
+
+    echo "Idoso é maior de idade <br>";
+
+}else{
+
+    echo "Velho é menor de idade";
+    
+}
+?>
