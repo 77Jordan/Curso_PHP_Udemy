e42fbdad380fec2be15fd0026533d6fcdb22f6d6
Sd Inf JORDAN <eb0118706472@eceme.eb.mil.br>
2024-10-28 10:34:05 -0300

    ain painho

---
+++ 7_estrutura_de_repeticao/11_exercicio_30B/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+$vet = [];
+
+
+
+for ($i = 1; $i <= 10; $i++) {
+  
+    array_push($vet, $i);
+    
+}
+
+print_r($vet);
+
+?>
---
+++ 7_estrutura_de_repeticao/12_exercicio_30C/index.php
@@ -0,0 +1,28 @@
+<?php 
+
+$vet = [];
+
+
+
+for ($i = 10; $i <= 20; $i++) {
+
+    array_push($vet, $i);
+   
+}
+
+print_r($vet);
+echo "<br><br>";
+
+for ($i = 0; $i <= count ($vet); $i++) {
+
+    if ($vet[$i] % 2 != 0) {
+
+        echo "$vet[$i] É IMPAR <br>";
+
+    }
+
+    
+}
+
+
+?>
