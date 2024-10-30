bef5431c3e31c9729c9f6f43ceceff6c35609ee6
QUARTEL <jordangaldino@gmail.com>
2024-10-30 14:35:10 -0300

    1435

---
+++ 8_inclusao_de_arquivos/4_short_tags/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+echo "TESTANDO CÓDIGO PHP NAS TAGS NORMAIS <br>";
+
+?>
+
+<? 
+
+echo "TESTANDO CÓDIGO PHP COM SHORT TAGS <br>";
+
+?>
---
+++ 8_inclusao_de_arquivos/5_exibicao_de_conteudo/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+    $nome = "Jordinho";
+    $sobrenome = "Araujo";
+
+?> 
+
+<form action="">
+    <div>
+        <input type="text" value="<?= $nome; ?>">
+    </div>
+    <div>
+        <input type="text" value="<?= $sobrenome; ?>">
+    </div>
+    <div>
+    <input type="submit" value="Enviar">
+    <div>
+</form>
