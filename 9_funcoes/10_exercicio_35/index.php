11a44d8a0e8a2e1114e232ca74665ba70397810b
QUARTEL <jordangaldino@gmail.com>
2024-11-07 15:20:29 -0300

    jesus morreu

---
+++ 9_funcoes/10_exercicio_35/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+    function quadrado($num) {
+
+        return $num * $num; 
+
+    }
+
+    $x = quadrado(10);
+
+    echo "o quadrado é $x <br>";
+
+    echo "O quadrado é " . quadrado(20) . "<br>";
+    echo "O quadrado é " . quadrado(30) . "<br>";
+    echo "O quadrado é " . quadrado(60) . "<br>";
+    echo "O quadrado é " . quadrado(90) . "<br>";
+
+?>
