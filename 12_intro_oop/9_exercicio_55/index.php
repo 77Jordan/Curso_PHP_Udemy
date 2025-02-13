3a6908666b9fcbef86cc12555d793412f94b3aed
Sd Inf JORDAN <eb0118706472@eceme.eb.mil.br>
2025-02-13 09:27:36 -0300

    gghhhh

--- 12_intro_oop/9_exercicio_55/index.php
+++ 12_intro_oop/9_exercicio_55/index.php
@@ -8,23 +8,35 @@ class Car {
     public $vel_max;
 
     function setVelMax($vel_max) {
-        $this->vel_max = $vel_max;
-       
+        $this->vel_max = $vel_max;   
     }
     function getVelMax() {
-         return $this->vel_max;
+        return $this->vel_max;
+    }
+    
+    function setCor($cor) {
+        $this->cor = $cor;   
+    }
+    function getCor() {
+        return $this->cor;
     }
 
-    
+    function setMarca($marca) {
+        $this->marca = $marca;   
+    }
+    function getMarca() {
+        return $this->marca;
+    }
+
 }
 
     $carro = new Car;
 
-    $carro->marca = "Ford";
-    $carro->cor = "Branco";
+    $carro->setMarca("Ford");
+    $carro->setCor("Branco");
     $carro->setVelMax(50);
 
-    echo "O carro possui a cor $carro->cor, é da marca $carro->marca e está a " . $carro->getVelMax() . "km/h <br>";
+    echo "O carro possui a cor " .  $carro->getCor() . ", é da marca " . $carro->getMarca() . " e está a " . $carro->getVelMax() . "km/h <br>";
     $carro->setVelMax(100);
     echo $carro->getVelMax() . "km/h <br>";
 ?>
